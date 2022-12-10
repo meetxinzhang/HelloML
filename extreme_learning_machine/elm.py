3491d8a19929c5b3956dd0ee2b66126e177125d0
Xin Zhang <MeetDevin.zh@outlook.com>
2022-12-10 14:53:13 +0800

    20221210

--- extreme_learning_machine/elm.py
+++ extreme_learning_machine/elm.py
@@ -4,41 +4,59 @@
  @contact: 2250271011@email.szu.edu.cn
  @time: 2022/12/9 20:42
  @desc:
+
+ ELM[1] was proposed for single-hidden layer feedforward neural networks (SLFNs) which randomly chooses hidden nodes and
+ analytically determines the output weights of SLFNs. In theory, this algorithm tends to provide good generalization
+ performance at extremely fast learning speed.
+
+ ELM include two layers, named hidden-layer and output-layer, I prefer to call them `random-features-mapping-layer` and
+ `learnable-layer`, since new names indicating their functions.
+
+ Ref.
+  [1] Huang, G et al,.  Extreme learning machine: theory and applications. Neurocomputing. 2006. 70(1/3), 489-501.
+
 """
 import numpy as np
 
 
 def sigmoid(v):
-    """activate function
+    """activation function, to offer the non-linear-mapping
     """
     return 1 / (1 + np.exp(-v))
 
 
 class ExtremeLearningMachine:
-    def __init__(self, in_features, out_features, hidden=64):
+    def __init__(self, in_features, out_features=1, hidden_features=64):
         self.f = in_features  # num of columns/attributes
-        self.o = out_features
-        self.h = hidden
+        self.o = out_features  # num of output dimension, default=1
+        self.h = hidden_features  # num of hidden layer, default=64
 
-        # hidden_layer: map features into another dimension, non-linear
-        self.weight = np.random.rand(self.f, self.h)
-        self.bias = np.random.rand(1, self.h)
+        # random-features-mapping-layer, non-linear
+        self.weight = np.random.rand(self.f, self.h)  # extend the dimension of features into h by matrix multiply.
+        self.bias = np.random.rand(1, self.h)  # bias added with each sample.
 
-        # output_layer: do prediction, linear
+        # learnable-layer: do prediction, linear, learnable.
         self.beta = np.ones(shape=[self.h, self.o])
 
     def random_feature_mapping(self, x):
         [batch, in_features] = np.shape(x)
         assert in_features == self.f
-        bias = np.repeat(self.bias, batch, axis=0)
-        features = sigmoid(x * self.weight + bias)
+        bias = np.repeat(self.bias, batch, axis=0)  # copy bias, [1,h] -> [batch,h]
+        features = sigmoid(x * self.weight + bias)  # [batch,f] * [f,h] + [batch,h] -> [batch,h]
         return features
 
     def train(self, x, y):
-        features = self.random_feature_mapping(x)
-        # Solve beta by Least Square
-        self.beta = features.I * y.T
+        """Solve beta
+        Define output=features*beat, loss=||output-y||^2
+        To minimize the loss, the output should close to y, that is: y~=features*beat
+        The idea of ELM is to let beat=features^-1*y, than you will see:
+              features*beta = features*features^-1*y = I*y = y
+        So, the key is to solve the inverse matrix of features (Moore-Penrose generalized inverse matrix).
+        """
+        features = self.random_feature_mapping(x)  # [batch,f] -> [batch,h]
+        inverse = features.I  # find the inverse
+        self.beta = inverse * y  # [h, batch] * [batch,o] -> [h,o]
 
-    def forward(self, x):
+    def predict(self, x):
         features = self.random_feature_mapping(x)
-        return features * self.beta
+        return features * self.beta  # [batch,h] * [h,o] -> [batch,o]
--- extreme_learning_machine/main.py
+++ extreme_learning_machine/main.py
@@ -1,98 +1,46 @@
 import numpy as np
+import csv
 
 
-def load_data():
-    """
-    加载本地数据
-    :return:
-    """
-    train_x = []
-    train_y = []
-    fileIn = open('container_crane_controller.txt')
-    for line in fileIn.readlines():
-        lineArr = line.strip().split(',')
-        train_x.append([float(lineArr[0]), float(lineArr[1])])
-        train_y.append(float(lineArr[2]))
-
-    return np.mat(train_x), np.mat(train_y)
+# def load_txt(path):
+#     x = []
+#     y = []
+#     with open(path) as file:
+#         for line in file.readlines():
+#             line = line.strip().split(',')
+#             x.append([float(line[0]), float(line[1])])
+#             y.append(float(line[2]))
+#     return np.mat(x), np.mat(y).T
 
 
-x, y = load_data()
-[r, c] = x.shape
-print('x.shape: ', r, c)
-h = 15
+iris_dic = {'Iris-setosa': 0,
+            'Iris-versicolor': 1,
+            'Iris-virginica': 2}
 
 
-# from extreme_learning_machine.elm import ExtremeLearningMachine
-# elm = ExtremeLearningMachine(in_features=2, out_features=1, hidden=15)
-# elm.train(x, y)
-# _y = elm.forward(x)
-# print(_y.T)
+def load_csv(path):
+    x = []
+    y = []
+    with open(path) as file:
+        for i, row in enumerate(csv.reader(file)):
+            if i==0: continue
+            x.append([float(e) for e in row[:-1]])
+            y.append(iris_dic[row[-1]])
+    return np.mat(x), np.mat(y).T
 
 
-def sigmoid(v):
-    """激活函数
-    """
-    return 1 / (1 + np.exp(-v))
+x, y = load_csv('Iris.csv')
+# x, y = load_txt('container_crane_controller.txt')
+print('x.shape: ', x.shape)
+print('y.shape: ', y.shape)
+
+from extreme_learning_machine.elm import ExtremeLearningMachine
+elm = ExtremeLearningMachine(in_features=5, out_features=1, hidden_features=32)
+elm.train(x, y)
+_y = elm.predict(x)
 
 
-# initiate the weight and bias by randomness.
-W = np.random.rand(c, h)
-b = np.random.rand(1, h)
-
-# np.repeat 对行进行复制，如下：
-# >>> a=np.array([[10,20],[30,40]])
-#
-# >>> a.repeat([3,2],axis=0)
-#
-# array([[10, 20],
-#
-#          [10, 20],
-#
-#          [10, 20],
-#
-#          [30, 40],
-#
-#          [30, 40]])
-# 复制 N 次，从而与输入数据一一对应
-b = np.repeat(b, r, axis=0)
-
-# map the x into high-dimensional space.
-hid = sigmoid(x * W + b)
-yT = y.T
-
-# m = 2
-# T = np.zeros((N, m))
-# for i in xrange(N):
-#     if train_y[0, i] == 0:
-#         T[i, 0] = 1
-#     else:
-#         T[i, 1] = 1
-
-# 最小二乘求解
-outputWeight = hid.I * yT
-output = hid * outputWeight
-
-print('输出层权值')
-print(outputWeight.T)
-print('预测结果')
-print(output.T)
-print('真实结果')
-print(y)
-
-# print(np.sum((output > 0.5) == train_y.T) * 1.0 / N)
-# print(np.sum(np.argmax(output, axis=1) == train_y.T) * 1.0 / N)
-
-#
-# a = [[1, 2, 3],
-#      [4, 5, 6],
-#      [7, 8, 9]]
-# a = np.mat(a)
-# print(a.I)
-#
-# aT = np.transpose(a)
-# aTa = np.dot(aT, a)
-#
-# p = np.linalg.inv(aTa)
-# paT = np.dot(p, aT)
-# print(paT)
+print('\nPrediction')
+print(_y.T)
+print('Label')
+print(y.T)
